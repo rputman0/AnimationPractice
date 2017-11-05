bc5478efc44b401b9c9cf03e67cf2446af42db3f
rputman0 <rputman0@gmail.com>
2017-11-05 18:14:18 -0500

    Update Pentagon.py
    -fix bug, where screen still had some of the old characters in it

--- Pentagon.py
+++ Pentagon.py
@@ -1,39 +1,36 @@
-import pygame, sys
+import sys, pygame
+from pygame.locals import *
 
 pygame.init()
+size = width, height = 300,100
+screen = pygame.display.set_mode(size)
 
-#set up the window
-width,height = 300,300
-screen = pygame.display.set_mode((width, height), 0, 32)
-pygame.display.set_caption('Draw Pentagon')
+pygame.display.set_caption("Font")
+myfont = pygame.font.SysFont("sans-serif",48)
 
-#have the default values be offscreen
-vertices = [(-10,-10),(-10,-10),(-10,-10),(-10,-10),(-10,-10)]
+WHITE = (255,255,255)
+BLACK = (0,0,0)
 
-iter = 0
-screen.fill((255,255,255))
+text = "Hello World "
 
 while True:
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             sys.exit()
-            
-    #ask the user for five input points to draw a polygon
-    if(event.type == pygame.MOUSEBUTTONDOWN and iter < 5):
-        #click the window at five random locations on the screen
-            point = pygame.mouse.get_pos()
-            vertices[iter] = point
-            pygame.draw.circle(screen,(0,0,255),point,5,0)
-            pygame.display.update()
-            #ask user for different inputs, rather than checking whenever the screen refreshes
-            pygame.time.wait(300)
-            iter += 1
-    #draw the polygon
-    elif(iter >= 5):
-        pygame.draw.polygon(screen,(255,165,0),vertices,0)
-        pygame.display.update()
+
+    screen.fill(BLACK)
+
+    text = text[len(text)-1]+ text[0:len(text)-1]
+    txtRot = myfont.render(text,1,WHITE)
+    screen.blit(txtRot, (5,10))
     
-    pygame.display.update()
- 
+    pygame.time.wait(600)
+
+    #make rotating animation
+    #by putting the last character at the begining of the word
+
+        #rotate -> erotat -> terota ->
+
 pygame.exitonclose()
+
